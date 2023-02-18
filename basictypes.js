920f5528fd7d147a982d945eadcb2f9c36ca44f1
JoowonKim <99549623+joowining@users.noreply.github.com>
2023-02-18 21:53:44 +0900

    ch2 basictypes

---
+++ basictypes.js
@@ -0,0 +1,27 @@
+//number
+let intNum = 10;
+let floatNum = 0.1;
+
+//string
+let singleQuoteStr = 'single quote string';
+let doubleQutoeStr = "double quote string";
+let singleChar = 'a';
+
+//bool
+let boolVar = true;
+
+//undefined
+let emptyVar;
+
+//null
+let nullVar = null;
+
+console.log(
+    typeof intNum,
+    typeof floatNum,
+    typeof singleQuoteStr,
+    typeof doubleQutoeStr,
+    typeof boolVar,
+    typeof emptyVar,
+    typeof nullVar
+)
